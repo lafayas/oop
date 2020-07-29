9457eeee55d52a25ec2732f72b87232e3663c57c
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-07-29 07:30:32 +0000

    function-2-1.cpp1

---
+++ practical-01/function-2-1.cpp
@@ -0,0 +1,17 @@
+// function to ...
+int minimum(int array[], int n)
+{
+	if (n < 1)
+{
+	return 0;
+}
+	int min = array[0];
+	for (int i = 0; i < n; i++)
+{
+	if (min > array[i])
+{
+	min = array[i];
+}
+}
+	return min;
+}
