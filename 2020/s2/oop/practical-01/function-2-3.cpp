b4ad4952c09ba5051056d97574947ddd1db222ee
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-07-29 08:09:20 +0000

    function-2-3.cpp1

---
+++ 2020/s2/oop/practical-01/function-2-3.cpp
@@ -0,0 +1,22 @@
+void twofivenine(int array[], int n)
+{
+	int num_twos = 0;
+	int num_fives = 0;
+	int num_nines = 0;
+	for (int i = 0; i < n; i++)
+{
+	if (array[i] == 2)
+{
+	num_twos = num_twos + 1;
+}
+else if (array[i] == 5)
+{
+	num_fives = num_fives + 1;
+}
+else if (array[i] == 9)
+{
+	num_nines = num_nines + 1;
+}
+}
+	
+}
