80e75f2a073749182e23a225610a9b09a186cede
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-07-30 01:08:26 +0000

    asd

---
+++ 2020/s2/oop/practical-01/function-3-1.cpp
@@ -0,0 +1,28 @@
+// function to ...
+bool fanarray(int array[], int n)
+{
+	bool x = true;
+	for (int i = 0; i < n / 2; i++)
+	{
+	    if (array[i + 1] < array[i] || array[i] != array[n - i - 1])
+	    {
+	        x = false;
+	    }
+	}
+	bool y = false;
+	for (int i = n / 2; i < n - 1; i++)
+	{
+	    if (array[i + 1] < array[i])
+	    {
+	       y = true;
+	    }
+	}
+	if (y == true && x == true)
+	{
+	    return true;
+	}
+	else
+	{
+	    return false;
+	}
+}
