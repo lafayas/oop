b69c6099aeffc536818760b05b2d48e52989a3cc
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-07-30 04:04:43 +0000

    asdasdp

--- 2020/s2/oop/practical-01/function-3-3.cpp
+++ 2020/s2/oop/practical-01/function-3-3.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-int main(){
+#include <stdlib.h>
+int main()
+{
 
     int a[] = {1,2,3,4,4,3};
 
@@ -7,11 +9,13 @@ int main(){
     int len=7,i=0,j=0,temp=0;
     int count [10000];
 
-    for (i = 0;i < len;i++){
+    for (i = 0;i < len;i++)
+    {
         temp  = a[i];
         count[temp]++;  //66666  
     }  
-    for(j = 1;j <= len;j++){
+    for(j = 1;j <= len;j++)
+    {
         std::cout << count[j] << std::endl;
     }
 
