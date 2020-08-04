0e633da7b6a68aaac61b27ab0235bab7c3379e3a
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-04 07:24:19 +0000

    pop

--- workshop-02/transform.cpp
+++ workshop-02/transform.cpp
@@ -2,17 +2,11 @@
 using namespace std;
 void transform(int n)
 {
-    for (int i = 16; i >= 0; i--)
+    for (int i = 10; i >= 0; i--)
     {
         if (n & (1 << i))
             cout << "1";
         else
             cout << "0";
     }
-}
-
-int main()
-{
-    transform(90);
-    return 0;
 }
