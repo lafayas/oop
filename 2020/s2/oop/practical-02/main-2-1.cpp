b453e5e7c032dd09d45444837945729ed199b87a
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-05 07:38:19 +0000

    as

---
+++ 2020/s2/oop/practical-02/main-2-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+
+extern void print_as_binary(std::string decimal_number);
+
+int main(int argc, char **argv)
+{
+    std::string decimal_number = "23";
+    print_as_binary(decimal_number);
+	return 0;
+}
