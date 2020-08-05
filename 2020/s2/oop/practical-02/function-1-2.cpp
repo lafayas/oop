a8196242c420bf4bedc496f4c02401597c67afba
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-05 06:31:49 +0000

    asddsap

--- 2020/s2/oop/practical-02/function-1-2.cpp
+++ 2020/s2/oop/practical-02/function-1-2.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 int identity(int array[10][10])
-{
+{   
+    int count = 0;
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
-        { 
-            if (array[i][j] == 1 && array[i][i] == 0 && array[j][j] == 0)
+        {
+            if (i == j)
             {
-                return true;
+                if (array[i][j] != 1)
+                {
+                    return false;
+                }
+            }
+            else if (i != j)
+            {
+                if (array[i][j] != 0)
+                {
+                    return false;
+                }
             }
             else
-            return false;
+            return true;
         }
     }
     return 0;
