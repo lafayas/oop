0a7e427a43a8fd33d4a18bd9bf5273b62e21aef7
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-06 03:11:34 +0000

    pop

---
+++ 2020/s2/oop/practical-02/function-2-3.cpp
@@ -0,0 +1,59 @@
+bool is_a_palindrome(int integers[], int length)
+{
+    if (length <= 0)
+    {
+        return -1;
+    }
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (integers[i + 1] >= integers[i] && integers[i] == integers[length - i - 1])
+        {
+            return integers[i];
+        }
+        else
+        {
+            return -2;
+        }
+    }
+}
+
+int sum_elements(int integers[], int length)
+{   
+    if (length <= 0)
+    {
+        return -1;
+    }
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += integers[i];
+    }
+    return sum;
+}
+
+int sum_if_a_palindrome(int integers[], int length)
+{
+    if (length <= 0)
+    {
+        return -1;
+    }
+    int sum = 0;
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (integers[i + 1] >= integers[i] && integers[i] == integers[length - i - 1])
+        {
+            for (int i = 0; i < length; i++)
+            {   
+                sum += integers[i];
+            }
+                return sum;
+        }
+        else
+        {
+            return -2;
+        }
+    }
+
+
+}
+
---
+++ 2020/s2/oop/practical-02/main-2-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+extern bool is_a_palindrome(int*, int);
+extern int sum_elements(int*, int);
+extern int sum_if_a_palindrome(int*, int);
+int main(int argc,char **argv)
+{
+    int integers[5] = {1, 4, 3, 2, 2};
+    std::cout << is_a_palindrome(integers, 5) << std::endl;
+    std::cout << sum_elements(integers, 5) << std::endl;
+    std::cout << sum_if_a_palindrome(integers, 5) << std::endl;
+    return 0;
+}
