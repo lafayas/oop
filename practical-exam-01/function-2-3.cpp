a22eeb702c8dd2a6adf8cbd45a82d0ac0b165ea7
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-12 06:15:48 +0000

    pp

--- practical-exam-01/function-2-3.cpp
+++ practical-exam-01/function-2-3.cpp
@@ -26,7 +26,7 @@ int sum_elements(int integers[], int length)
     int sum = 0;
     for (int i = 0; i < length; i++)
     {
-        sum += integers[i];
+        sum = integers[i] + sum;
     }
     return sum;
 }
@@ -42,9 +42,9 @@ int sum_if_a_palindrome(int integers[], int length)
     {
         if (integers[i + 1] >= integers[i] && integers[i] == integers[length - 1 - i])
         {
-            for (int i = 0; i <length; i++)
+            for (int i = 1; i <length; i++)
             {
-                sum += integers[i];
+                sum = integers[i] + sum;
             }
             return sum;
         }
