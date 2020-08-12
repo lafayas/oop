3642e03efa28a48181b97bb62659d7637adad528
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-12 06:14:08 +0000

    pp

--- 2020/s2/oop/practical-exam-01/function-2-3.cpp
+++ 2020/s2/oop/practical-exam-01/function-2-3.cpp
@@ -6,7 +6,7 @@ bool is_a_palindrome(int integers[], int length)
     }
     for (int i = 0; i < length / 2; i++)
     {
-        if (integers[i + 1] >= integers[i] && integers[i] == integers[length - i - 1])
+        if (integers[i + 1] >= integers[i] && integers[i] == integers[length - 1 - i])
         {
             return integers[i];
         }
@@ -18,7 +18,7 @@ bool is_a_palindrome(int integers[], int length)
 }
 
 int sum_elements(int integers[], int length)
-{   
+{
     if (length <= 0)
     {
         return -1;
@@ -40,20 +40,17 @@ int sum_if_a_palindrome(int integers[], int length)
     int sum = 0;
     for (int i = 0; i < length / 2; i++)
     {
-        if (integers[i] == integers[length - i - 1])
+        if (integers[i + 1] >= integers[i] && integers[i] == integers[length - 1 - i])
         {
-            for (int i = 0; i < length; i++)
-            {   
+            for (int i = 0; i <length; i++)
+            {
                 sum += integers[i];
             }
-                return sum;
+            return sum;
         }
         else
         {
             return -2;
         }
     }
-
-
 }
-
