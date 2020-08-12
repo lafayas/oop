fd960c0cac100cf8230aa474b2bed7ebf97951a9
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-12 06:02:46 +0000

    12

--- 2020/s2/oop/practical-exam-01/function-1-1.cpp
+++ 2020/s2/oop/practical-exam-01/function-1-1.cpp
@@ -4,18 +4,17 @@ void printer(int array[10][10])
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
-        {   
-            if (i <= 9 && j < 9)
+        {
+            if (i <= 9 && j <9)
             {
-        
                 std::cout << array[i][j] << " ";
+                
             }
-            else
+            else 
             {
                 std::cout << array[i][j];
             }
         }
-        std::endl;
+        std::cout << "\n";
     }
-    
 }
