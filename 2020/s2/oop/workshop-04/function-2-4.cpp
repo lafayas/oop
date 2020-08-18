43f30877ecf1ce0ef3ee378e82ce21f59a1b104d
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-18 07:43:51 +0000

    asd

--- 2020/s2/oop/workshop-04/function-2-4.cpp
+++ 2020/s2/oop/workshop-04/function-2-4.cpp
@@ -1,6 +1,12 @@
 float multiply_op(float left, float right)
 {
+
     float res;
     res = left * right;
     return res;
+}
+
+float arithmetic_ops(float left, float right, float (*op)(float,float))
+{
+		return (*op)(left, right);
 }
