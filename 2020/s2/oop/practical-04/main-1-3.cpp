dbffabd8416794098c1c7dae46e4e8c994042ff0
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-19 01:55:39 +0000

    a

--- 2020/s2/oop/practical-04/main-1-3.cpp
+++ 2020/s2/oop/practical-04/main-1-3.cpp
@@ -5,8 +5,8 @@ extern void cpyia(int old_array[],int new_array[],int length);
 int main()
 {   
     int length = 3;
-    int old_array[length] = {1, 2, 3};
-    int new_array[length];
+    int old_array[] = {1, 2, 3};
+    int new_array[3];
     cpyia(old_array, new_array, length);
     return 0;
 }
