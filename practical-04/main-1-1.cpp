57a0b6580f0a83e35c97fb373da599609633988c
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-19 01:46:41 +0000

    a

--- practical-04/main-1-1.cpp
+++ practical-04/main-1-1.cpp
@@ -4,9 +4,9 @@ extern void copy_2d_strings(string first[][2], string second[][2], int n);
 
 int main()
 {
-    int n = 3;
-    string first[3][2] = {"a", "b", "c", "d", "e", "f"};
-    string second[3][2];
+    const int n = 3;
+    string first[n][2] = {"a", "b", "c", "d", "e", "f"};
+    string second[n][2];
     copy_2d_strings(first, second, n);
     return 0;
 }
