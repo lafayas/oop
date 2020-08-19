0e63c1b6457e5bff852eda5608c66c835e3f5cf1
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-19 02:12:06 +0000

    a

---
+++ 2020/s2/oop/practical-04/main-2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+extern void print_sevens(int *nums,int length);
+
+int main()
+{   
+    int length = 3;
+    int nums[length] = {7, 14, 21};
+    print_sevens(nums, length);
+    return 0;
+}
