aaa54e89e1034ca5b648ee1ae755d12ae71d3b21
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-19 01:47:12 +0000

    a

--- 2020/s2/oop/practical-04/function-1-1.cpp
+++ 2020/s2/oop/practical-04/function-1-1.cpp
@@ -9,9 +9,9 @@ void copy_2d_strings(string first[][2], string second[][2], int n)
         for (int j = 0; j < 2; j++)
         {
             second[i][j] = first[i][j]; 
-            cout<< second[i][j];
+            
         }
     }
-    cout<<endl;
+    
     
 }
