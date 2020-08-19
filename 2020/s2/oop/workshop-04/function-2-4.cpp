f98940425c45792acbdd90f9d91f973a48af9b45
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-19 02:22:14 +0000

    a

--- 2020/s2/oop/workshop-04/function-2-4.cpp
+++ 2020/s2/oop/workshop-04/function-2-4.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+using namespace std;
+
 float multiply_op(float left, float right)
 {
 
@@ -6,7 +12,21 @@ float multiply_op(float left, float right)
     return res;
 }
 
+float add_op(float left, float right)
+{   
+    float res;
+    res = left + right;
+    return res;
+}
+
+float subtract_op(float left, float right)
+{
+    float res;
+    res = left - right;
+    return res;
+}
+
 float arithmetic_ops(float left, float right, float (*op)(float,float))
 {
-		return (*op)(left, right);
+		return op(left, right);
 }
--- 2020/s2/oop/workshop-04/main-2-4.cpp
+++ 2020/s2/oop/workshop-04/main-2-4.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
 
+extern float add_op(float left, float right); 
+extern float subtract_op(float left, float right);
 extern float multiply_op(float left, float right);
+extern float arithmetic_ops(float left, float right, float (*op)(float,float));
+
 
 int main()
-{
-    float left;
-    cout<< "Enter the first number: ";
-    cin>> left;
-    float right;
-    cout<< "Enter the second number: ";
-    cin>> right;
-    
-    cout<< multiply_op(left, right) << endl;
+{   
+    string op;
+    float left = 3.6;
+    float right = 2.7;
+    cout<< "Enter the operator: ";
+    cin>> op;
+    if (op == "+")
+    {
+        cout<<arithmetic_ops(left, right, &add_op)<<endl;
+    }
+    else if (op == "-")
+    {
+        cout<<arithmetic_ops(left, right, &subtract_op)<<endl;
+    }
+    else if (op == "*")
+    {
+        cout<<arithmetic_ops(left, right, &multiply_op)<<endl;
+    }
     return 0;
 }
