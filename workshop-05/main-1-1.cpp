2d381ebb7e567a44ce374d4feca9c2c248d8bd18
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-24 15:03:13 +0000

    a

--- workshop-05/main-1-1.cpp
+++ workshop-05/main-1-1.cpp
@@ -5,6 +5,6 @@ extern void printNumbers(int *numbers,int length);
 
 int main()
 {   
-    int array = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10};
+    int array[20] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10};
     return array;
 }
