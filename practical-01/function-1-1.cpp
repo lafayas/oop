90e710ee7ad7d354be47f947e7b69e1d7c474f2f
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-24 14:45:32 +0000

    a

--- practical-01/function-1-1.cpp
+++ practical-01/function-1-1.cpp
@@ -1,1 +1,15 @@
-return 0;
+// function to ...
+int sum_array(int array[], int n)
+{
+  int sum = 0;
+  if (n < 1)
+{
+  return sum;
+}
+  
+  for (int i = 0; i < n; i++)
+{
+  sum += array[i-1];
+}
+  return sum;
+}
