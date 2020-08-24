898aeeda5731c6a70c4484ed1641a781add1cb72
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-24 15:23:49 +0000

    a

--- 2020/s2/oop/practical-01/function-1-2.cpp
+++ 2020/s2/oop/practical-01/function-1-2.cpp
@@ -1,15 +1,5 @@
 //function to ...
 double average(int array[], int n)
 {
-	double sum = 0;
-	if (n < 1)
-{
-	return 0;
-}
-	for (int i = 0; i < n; i++)
-{
-	sum += array[i];
-}
-	double average = sum / n;
-	return average;
+	return 20;
 }
