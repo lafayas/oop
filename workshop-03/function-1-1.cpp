35a7b9c9bd730b9e6a27dd92f6df931b731cdf39
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-24 14:52:19 +0000

    a

--- workshop-03/function-1-1.cpp
+++ workshop-03/function-1-1.cpp
@@ -5,22 +5,7 @@ Signature: void printer(int array[10][10]);*/
 using namespace std;
 void printer(int array[10][10])
 {
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {   
-            if (i <= 9 && j < 9)
-            {
-        
-                cout << array[i][j] << " ";
-            }
-            else
-            {
-                cout << array[i][j];
-            }
-        }
-        cout << "\n";
-    }
+    cout << "\n";
     
 }
 
