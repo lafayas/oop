a03786e803203206f72d37740e644060a82334e3
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-25 07:34:05 +0000

    a

--- 2020/s2/oop/workshop-05/function-3-1.cpp
+++ 2020/s2/oop/workshop-05/function-3-1.cpp
@@ -19,37 +19,31 @@ int *readNumbers()
 	return readNumbers;
 }
 
-void printNumbers(int *numbers,int length)
+/*void printNumbers(int *numbers,int length)
 {
 	for (int i = 0; i < length; i++)
 	{
 		cout<<i<<" "<<*(numbers+i)<<endl;
 	}
-}
+}*/
 
 bool equalsArray(int *numbers1,int *numbers2,int length)
 {   
-    bool equalsArray = 0;
-    int counter = 0;
+  
     
     if (length < 1)
     {
-        return equalsArray;
+        return false;
     }
     
     for (int i = 0; i < length; i++)
 	{
-		if (*(numbers1 + i) == *(numbers2 + i))
-			counter = counter+1;
+		if (*(numbers1 + i) != *(numbers2 + i))
+		{
+		    return false;
+		}
+			
 	}
 	
-	if (counter == length)
-	{
-		equalsArray = 1;
-		return equalsArray;	
-	}
-	else
-	{
-		return equalsArray;
-	}
+    return true;
 }
