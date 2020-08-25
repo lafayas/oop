9434e9c1e630dacfaaeab90885f928a2fd59032a
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-25 07:52:40 +0000

    a

--- 2020/s2/oop/workshop-05/function-3-2.cpp
+++ 2020/s2/oop/workshop-05/function-3-2.cpp
@@ -39,3 +39,22 @@ bool equalsArray(int *numbers1,int *numbers2,int length)
 	
     return true;
 }
+
+int *reverseArray(int *numbers1,int length)
+{
+	int *reverseArray = new int [length];
+	int n = length;
+	
+	if (length < 1)
+	{
+		return 0;
+	}
+
+	for (int i = 0; i < length; i++)
+	{
+		n = n - 1;
+		*(reverseArray + n) = *(numbers1 + i);
+	}
+	
+	return reverseArray;
+}
