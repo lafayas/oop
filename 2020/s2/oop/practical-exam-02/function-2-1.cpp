fe9c07b1324f262437f6d2a843144be550c980b7
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-26 06:07:02 +0000

    a

---
+++ 2020/s2/oop/practical-exam-02/function-2-1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int *createArray(int n)
+{
+    int *createArray = new int [n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin>> *(createArray + i);
+    }
+    return createArray;
+}
+
+void displayArray(int *numbers, int length)
+{
+    for (int i = 0; i < length; i++)
+  {
+    cout<<i<<" "<<*(numbers+i)<<endl;
+  }
+}
+void bmiIndex(int *weight)
+{
+    int length = 4;
+    string *weight0 = new string [length];
+    
+    for (int i = 0; i < length; i++)
+    {
+        switch(weight[i])
+        {
+            case 18:
+            weight0[i] = "bit underweight";
+            break;
+            case 24:
+            weight0[i] = "Healthy weight!";
+            break;
+            case 29:
+            weight0[i] = "That's overweight.";
+            break;
+            case 39:
+            weight0[i] = "Looks like obese";
+            break;
+            default:
+            weight0[i] = " ";
+            break;
+            
+        }
+    }
+    for (int i = 0; i < length; i++)
+    {
+         cout<<i<<" "<<*(weight+i)<<" "<<*(weight0+i)<<endl;
+    }
+}
