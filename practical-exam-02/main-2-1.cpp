dc97fcc72c01e64bf67e265491f5528afdbac3ae
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-26 06:07:02 +0000

    a

---
+++ practical-exam-02/main-2-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+extern int *createArray(int n);
+extern void displayArray(int *weight, int length);
+extern void bmiIndex(int *weight);
+
+int main()
+{
+    
+    int length = 4;
+    int n = 4;
+    int *weight = new int [length];
+  
+  
+    weight = createArray(length);
+  
+  
+    bmiIndex(weight);
+  
+  
+    delete [] weight;
+  
+  return 0;
+}
