ddecad3721eb5917d0768cbceee85e796db971c6
a1807878 <a1807878@81a680f5-91d9-4ce7-9388-8c4002f8c7f3>
2020-08-26 06:40:49 +0000

    a

---
+++ practical-exam-02/function-4-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int *createArray(int n)
+{
+    int *createArray = new int [n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin>> *(createArray + i);
+    }
+    return createArray;
+}
+
+void displayArray(int *numbers, int length)
+{
+    for (int i = 0; i < length; i++)
+  {
+    cout<<i<<" "<<*(numbers+i)<<endl;
+  }
+}
+
+void printSubArrays(int *numbers, int length)
+{   
+    int s = 0
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = i; j < length; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                s = s + numbers[i];
+            }
+        }
+    }
+}
